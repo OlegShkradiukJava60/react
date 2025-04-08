21e10839129bb458115992e16a18ff345193ac85
Olegshkradiuk <Olegwills40@gmail.com>
2025-04-08 20:13:56 +0300

    hw react

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalId); // очистка при размонтировании
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <div className="current-time">
+          <h2>Текущее время:</h2>
+          <div className="time-display">{(time)}</div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
